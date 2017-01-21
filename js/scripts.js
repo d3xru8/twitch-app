5323cff1b068442bb2826bcd787f62a63b4f606d
Dexter Rualo <dexterrualo@gmail.com>
2017-01-21 23:13:59 +0800

    Still fixing api call

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
   var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   channels.forEach(function(channel){
     $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + channel, function(json){
+      $("<div class='item'></div>").appendTo(".list").click(function(){
+        window.open("https://www.twitch.tv/" + json.name);
+      }).append("<img src='" + json.logo + "' class='logo' />").append("<div>" + json.display_name + "</div>").append("<div class='game-status'></div>");
     });
     $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel, function(json){
+      if (json.stream) {
+        console.log(json.stream.channel);
+        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>" + json.stream.channel.game + "</div>").append("<div class='status'>" + json.stream.channel.status + "</div>");
+      }
     });
   });
 });
