6ef7f8ffbb5b6adeb0c28ecd5707d07a30dae6ea
Dexter Rualo <dexterrualo@gmail.com>
2017-01-22 01:39:27 +0800

    modified script

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
   channels.forEach(function(channel){
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + channel, function(json){
-      $("<div class='item'></div>").appendTo(".list").click(function(){
-        window.open("https://www.twitch.tv/" + json.name);
-      }).append("<img src='" + json.logo + "' class='logo' />").append("<div>" + json.display_name + "</div>").append("<div class='game-status'></div>");
-    });
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel, function(json){
-      if (json.stream) {
-        console.log(json.stream.channel);
-        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>" + json.stream.channel.game + "</div>").append("<div class='status'>" + json.stream.channel.status + "</div>");
+    $.when($.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + channel), $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel)).then(function(user,stream){
+      var userdata = user[2].responseJSON;
+      console.log(userdata);
+      if (!userdata.error) {
+        $("<div class='item'></div>").appendTo(".list").click(function(){
+          window.open("https://www.twitch.tv/" + userdata.name);
+        }).append("<img src='" + userdata.logo + "' class='logo' />").append("<div>" + userdata.display_name + "</div>");
+      } else {
+        $("<div class='item'></div>").appendTo(".list").append("<div class='error'>" + userdata.error + "</div>").append("<div class='message'>" + userdata.message + "</div>");
+      }
+      var streamdata = stream[2].responseJSON.stream;
+      console.log(streamdata);
+      if (streamdata) {
+        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>" + streamdata.channel.game + "</div>").append("<div class='status'>" + streamdata.channel.status + "</div>");
+      } else if (!userdata.error) {
+        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>Offline</div>").append("<div class='status'>Currently not streaming</div>");
       }
     });
   });
