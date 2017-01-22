e5de7d80485968f1fdb9bfae307ac5e4de486f0b
Dexter Rualo <dexterrualo@gmail.com>
2017-01-22 11:35:35 +0800

    Updated index.html styles and script

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,20 @@ $(document).ready(function(){
       if (!userdata.error) {
         $("<div class='item'></div>").appendTo(".list").click(function(){
           window.open("https://www.twitch.tv/" + userdata.name);
-        }).append("<img src='" + userdata.logo + "' class='logo' />").append("<div>" + userdata.display_name + "</div>");
+        }).append("<img src='" + userdata.logo + "' class='logo' />").append("<div class='display-name'>" + userdata.display_name + "</div>");
       } else {
-        $("<div class='item'></div>").appendTo(".list").append("<img class='logo' src='images/placeholder.png' />").append("<div class='error'>" + userdata.error + "</div>").append("<div class='message'>" + userdata.message + "</div>");
+        $("<div class='item not-found'></div>").appendTo(".list").append("<img class='logo' src='images/placeholder.png' />").append("<div class='display-name non-user'>" + channel + "</div>");
       }
       var streamdata = stream[2].responseJSON.stream;
       console.log(streamdata);
       if (streamdata) {
-        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>" + streamdata.channel.game + "</div>").append("<div class='status'>" + streamdata.channel.status + "</div>");
+        $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>Streaming: " + streamdata.channel.game + "</div>").append("<div class='status'>" + streamdata.channel.status + "</div>");
       } else if (!userdata.error) {
         $("<div class='game-status'></div>").appendTo(".item:last").append("<div class='game'>Offline</div>").append("<div class='status'>Currently not streaming</div>");
+      } else {
+        $("<div class='game-status'></div>").appendTo(".item:last").click(function(){
+          this.closest(".item").remove();
+        }).append("<div class='game error'>" + userdata.error + "</div>").append("<div class='status message'>" + userdata.message + "<br>(Click to remove)</div>");
       }
     });
   });
